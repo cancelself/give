bf32c4cb527b107d1e8ee9b7d40c1c0957127318
cancelself <doug@purdy.im>
2022-07-26 20:52:19 -0700

    starting to use the new xrpl.js sdk

--- index.js
+++ index.js
@@ -1,9 +1,69 @@
-function donateXRP()
+console.log("meditate2donate");
+
+const read = require('readline');
+
+const rl = read.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.on('line', (line) => {
+    
+    const command = line.toLowerCase().trim()
+    line.su
+
+    switch(command) 
+    {
+      case "xrp":
+        break;
+        default:
+          break;
+    }
+    console.log(command)
+    const xrpl = require("xrpl");
+    const client = new xrpl.Client("wss://xrplcluster.com");
+    client.connect();
+    const secret = "";
+    const destination = "rDsbeomae4FXwgQTJp9Rs64Qg9vDiTCdBv";
+    const wallet = xrpl.Wallet.fromSeed(secret);
+    console.log(wallet.address);
+
+  });
+
+async function donateXRP()
 {
 
 }
 
-function donateSolana()
-{
 
-}
+
+    /*const transaction = client.autofill(
+        {
+            "TransactionType": "Payment",
+            "Account": wallet.address,
+            //"LastLedgerSequence": client.getLedgerIndex() + 75, 
+            "Amount": 1600,
+            "Destination": destination
+            });
+
+      */
+     
+            const transaction = {
+                "TransactionType": "Payment",
+                "Account": wallet.address,
+                //"LastLedgerSequence": client.getLedgerIndex() + 75, 
+                "Amount": 1600,
+                "Destination": destination
+            }
+        
+        const tx = client.submitAndWait(transaction, { wallet: wallet });
+        console.log(tx);
+        
+        //client.disconnect();
+
+
+
+
+
+
+   
